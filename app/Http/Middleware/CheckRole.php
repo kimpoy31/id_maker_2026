3e80ebcc15b6c9dee566f293ac18178e0ee0052d
kimpoy31 <pinanonangkim000@gmail.com>
2026-09-02 14:44:55 +0800

    feat: add CheckRole middleware and register it
    - Create CheckRole middleware to restrict access to admin/editor roles
    - Register CheckRole middleware alias in bootstrap/app.php
    - Update templates routes to use CheckRole middleware

---
+++ app/Http/Middleware/CheckRole.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckRole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!auth()->check()) {
+            return redirect()->route('login');
+        }
+
+        $user = auth()->user();
+
+        // Allow access for admin and editor roles
+        if (!in_array($user->role, ['admin', 'editor'])) {
+            abort(403, 'You do not have permission to access this resource.');
+        }
+
+        return $next($request);
+    }
+}
--- routes/templates.php
+++ routes/templates.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\TemplateController;
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', 'CheckRole'])->group(function () {
     Route::get('/templates', [TemplateController::class, 'index'])->name('templates');
     Route::post('/templates', [TemplateController::class, 'store'])->name('templates.store');
 });
